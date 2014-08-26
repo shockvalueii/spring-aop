4ce639c3d713aaa8a96abb9ec1a823da440c3404
betheRed <futhanpl@gmail.com>
2014-08-26 17:10:49 +0700

    Second aspect: after aspect completed

--- SpringAOPdemo/src/com/theRed/aspect/BeforeAspect.java
+++ SpringAOPdemo/src/com/theRed/aspect/BeforeAspect.java
@@ -1,15 +1,25 @@
 package com.theRed.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class BeforeAspect {
-	@Before("execution(public void run())")
-	public void beforeMotoRunning(JoinPoint joinPoint) {
+	@Before("execution(public void  run())")
+	public void beforeVehicleRunning(JoinPoint joinPoint) {
 
-		System.out.println("before vehicle is running!");
+		System.out.println("before from: " + joinPoint.getSignature().getDeclaringType().toString());
+		System.out.println("hijacked : " + joinPoint.getSignature().getName());
+		System.out.println("******");
+	}
+	
+	@After("execution(public void run())")
+	public void afterVehicleRunning(JoinPoint joinPoint) {
+
+		System.out.println("******");
+		System.out.println("after from: " + joinPoint.getSignature().getDeclaringType().toString());
 		System.out.println("hijacked : " + joinPoint.getSignature().getName());
 		System.out.println("******");
 	}
